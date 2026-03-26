0567ea1e962482be0c0d1f34e631ab0c86941935
Anastasios Tsantoulis <Anastasios.TSANTOULIS@netcompany.com>
2026-03-26 12:42:23 +0200

    Update according to review

--- wallet-provider-service/src/main/kotlin/port/input/walletinstanceattestation/IssueWalletInstanceAttestation.kt
+++ wallet-provider-service/src/main/kotlin/port/input/walletinstanceattestation/IssueWalletInstanceAttestation.kt
@@ -104,7 +104,7 @@ value class WalletInstanceAttestationValidity(
     init {
         require(value.isPositive())
         require(value < ARF.MAX_WALLET_INSTANCE_ATTESTATION_VALIDITY) {
-            "validity must be less than ${ARF.MAX_WALLET_INSTANCE_ATTESTATION_VALIDITY}"
+            "Wallet Instance Attestation validity must be less than ${ARF.MAX_WALLET_INSTANCE_ATTESTATION_VALIDITY}"
         }
     }
 
